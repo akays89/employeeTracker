ee7e1e3555150515451fd322e7fb75a0cbbf032e
Amanda Kays <amanda@Amandas-MacBook-Air.local>
2021-07-28 17:37:15 -0500

    created connection

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const conTable = require('console.table');
 
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: process.env.DB_PASS,
+
+    database: "employees_db"
+});
