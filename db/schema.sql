bec0f852423ff111226df86277f8e8ba6aee15ca
Amanda Kays <amanda@Amandas-MacBook-Air.local>
2021-07-28 03:33:32 -0500

    created employee table

--- db/schema.sql
+++ db/schema.sql
@@ -10,9 +10,18 @@ CREATE TABLE department(
 );
 
 CREATE TABLE role(
-    id INTO AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY(id)
 );
