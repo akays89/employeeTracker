a90ce1ba83836602eff95d634c09bb3ed3e530c2
Amanda Kays <amanda@Amandas-MacBook-Air.local>
2021-07-28 16:13:39 -0500

    inseted data into empoyee table

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,3 +6,8 @@ VALUES ("sales"),("finance"), ("legal"),("engineering");
 INSERT INTO roles (title, salary,department_id)
 VALUES ("sales lead", 120000, 1), ("salesperson", 75000, 1),("lead engineer", 150000, 4),
 ("software engineer", 125000, 4), ("accountant", 120000, 2), ("lawyer", 190000, 3), ("lead attorney", 250000, 3);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ("Bobby", "Blue", 1, NULL), ("Wendy", "White", 2, 1), ("Joe", "Shmoe", 3, NULL), ("Candy", "Cane", 4, 3), 
+("Patrick", "Star", 5, NULL), ("Jane", "Doe", 6, 5);
+
