ea3bb4df63da38b670c78952d4e07980a8cf7ea7
Amanda Kays <amanda@Amandas-MacBook-Air.local>
2021-07-28 23:38:17 -0500

    created arawlist with inquirer for user to choose what theyd like to do

--- server.js
+++ server.js
@@ -21,9 +21,30 @@ var connection = mysql.createConnection({
     console.log("connected as id " + connection.threadId + "\n");
     console.log("Welcome to the Employee Tracking App..."); 
 
-   
+   start();
   });
 
+  function start() {
+
+    console.log(" "); 
+      inquirer
+      .prompt({
+        name: "actions",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: ["View all employees", 
+                  "View all roles", 
+                  "View all departments", 
+                  "Add an employee", 
+                  "Add a role", 
+                  "Add a department", 
+                  "Update an employee's role", 
+                  "Delete an employee", 
+                  "Delete a role", 
+                  "Delete a department",
+                  "Exit"]
+      })
+    }
 
 
     
